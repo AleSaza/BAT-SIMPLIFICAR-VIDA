73ff3e1aa183dc0a512f7f9f432e49647b6c5c19
Alexandre Pereira de Saza <49533149+AleSaza@users.noreply.github.com>
2023-12-20 13:12:10 -0300

    ABRIR LINKS POR MEIO DE BAT
    Esse é um exemplo de link que pode ser aberto através do bat.

---
+++ Clean All - History & Cache Cleaner.bat
@@ -0,0 +1,5 @@
+@echo off
+cd\
+cd Arquivos de Programas(x86)\Google\Chrome\Application
+
+start chrome.exe https://chromewebstore.google.com/detail/geschichte-und-cache-rein/pooaemmkohlphkekccfajnbcokjlbehk?pli=1
